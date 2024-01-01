07a1977a772448c74c8f8298fe1a842a24f502ea
BWallyn <ben96.wallyn@gmail.com>
2024-01-01 11:45:15 +0100

    Initial commit

---
+++ src/nyc_taxi/pipelines/data_engineering/download_data.py
@@ -0,0 +1,117 @@
+"""Functions to download datasets from webpage NYC TLC"""
+# =================
+# ==== IMPORTS ====
+# =================
+
+import gc
+import os
+from tqdm import tqdm
+from urllib.error import HTTPError
+
+import pandas as pd
+
+
+# ===================
+# ==== FUNCTIONS ====
+# ===================
+
+def download_dataset_yymm(link: str, year_month: str) -> pd.DataFrame:
+    """Download parquet dataset with the link and specific month
+
+    Args:
+        link: Link to the website containing the dataset
+        year_month: Specific month of the data to download. Written as yyyy-mm
+    Returns:
+        df: DataFrame of the NYC taxis
+    """
+    path = f'{link}{year_month}.parquet'
+    try:
+        df = pd.read_parquet(path=path)
+    except HTTPError as err:
+        if err.code == 403:
+            print(f'Dataset {year_month} not found')
+            df = pd.DataFrame()
+    return df
+
+
+def check_available(path_data: str, year_month: str) -> bool:
+    """Check if the file is available
+
+    Args:
+        path_data: Path to the datafiles
+        year_month: date to check
+    """
+    if os.path.isfile(os.path.join(path_data, f"nyc_taxi_{year_month}.pkl")):
+        return True
+    else:
+        return False
+
+
+def download_all_data(link: str, path_data: str, yyyy_start: int, yyyy_end: int, mm_start: int, mm_end: int) -> pd.DataFrame:
+    """Download all data between two dataset if not saved in the folder and saved them.
+
+    Args:
+        link: Webpage path to the NYC Taxi datasets
+        path_data: Path where to store datasets
+        yyyy_start: Starting year
+        yyyy_end: Ending year
+        mm_start: Starting month
+        mm_end: Ending month
+    Returns:
+        df: DataFrame merged of datasets between two datasets
+    """
+    # Create list of months
+    if yyyy_start == yyyy_end:
+        list_months = [
+            str(yyyy_start) + '-0' + str(month) if len(str(month)) == 1 else str(yyyy_start) + '-' + str(month) for month in range(mm_start, mm_end+1)
+        ]
+    else:
+        n_year_diff = yyyy_end - yyyy_start
+        for i in range(n_year_diff):
+            list_months = [
+                str(yyyy_start+i) + '-0' + str(month) if len(str(month)) == 1 else str(yyyy_start+i) + '-' + str(month) for month in range(mm_start, 13)
+            ]
+        list_months += [
+            str(yyyy_start) + '-0' + str(month) if len(str(month)) == 1 else str(yyyy_start) + '-' + str(month) for month in range(mm_start, mm_end+1)
+        ]
+    df = pd.DataFrame()
+    # Download dataset of each month
+    for year_month in list_months:
+        path_file = os.path.join(path_data, f"nyc_taxi_{year_month}.pkl")
+        if not os.path.isfile(path=path_file):
+            df_temp = pd.read_pickle(path_file)
+        else:
+            df_temp = download_dataset_yymm(link=link, year_month=year_month)
+            df_temp.to_pickle(os.path.join(path_data, f"nyc_taxi_{year_month}.pkl"))
+        df = pd.concat([df, df_temp])
+        del df_temp
+        gc.collect()
+    return df
+
+
+def merge_overall_dataset(link: str, yyyy_start: int, yyyy_end: int, mm_start: int, mm_end: int) -> pd.DataFrame:
+    """
+    """
+    # Create list of months
+    if yyyy_start == yyyy_end:
+        list_months = [
+            str(yyyy_start) + '-0' + str(month) if len(str(month)) == 1 else str(yyyy_start) + '-' + str(month) for month in range(mm_start, mm_end+1)
+        ]
+    else:
+        n_year_diff = yyyy_end - yyyy_start
+        for i in range(n_year_diff):
+            list_months = [
+                str(yyyy_start+i) + '-0' + str(month) if len(str(month)) == 1 else str(yyyy_start+i) + '-' + str(month) for month in range(mm_start, 13)
+            ]
+        list_months += [
+            str(yyyy_start) + '-0' + str(month) if len(str(month)) == 1 else str(yyyy_start) + '-' + str(month) for month in range(mm_start, mm_end+1)
+        ]
+    # download dataset of each month and merge
+    df = pd.DataFrame()
+    for year_month in tqdm(list_months):
+        df_tmp = download_dataset_yymm(link=link, year_month=year_month)
+        df = pd.concat([df, df_tmp])
+        del df_tmp
+        gc.collect()
+    df.reset_index(drop=True, inplace=True)
+    return df
---
+++ src/nyc_taxi/pipelines/data_engineering/nodes.py
@@ -0,0 +1,76 @@
+"""
+This is a boilerplate pipeline 'data_engineering'
+generated using Kedro 0.19.1
+"""
+# =================
+# ==== IMPORTS ====
+# =================
+
+import pandas as pd
+
+from .download_data import download_all_data
+
+
+# ===================
+# ==== FUNCTIONS ====
+# ===================
+
+def download_between_dates(link: str, date_start: str, date_end: str) -> pd.DataFrame:
+    """Download data of NYC taxi trips between two dates
+
+    Args:
+        link: Link to the NYC TLC website
+        date_start: Starting month to download data. Format yyyy-mm
+        date_end: Ending month to download data. Format yyyy-mm
+    Returns:
+        df: DataFrame downloaded
+    """
+    # Get parameters
+    yyyy_start = int(date_start[:4])
+    mm_start = int(date_start[5:])
+    yyyy_end = int(date_end[:4])
+    mm_end = int(date_end[5:])
+    # Extract data
+    df = download_all_data(link=link, yyyy_start=yyyy_start, yyyy_end=yyyy_end, mm_start=mm_start, mm_end=mm_end)
+    return df
+
+
+def split_train_val_test(df: pd.DataFrame, date_valid_str: str, date_test_str: str, col_date: str="tpep_pickup_datetime") -> list[pd.DataFrame]:
+    """Split the dataset into train, validation and test sets.
+
+    Args:
+        df: Entire dataframe
+    """
+    # Options
+    date_valid = pd.to_datetime(date_valid_str, errors="raise", format="%Y-%m")
+    date_test = pd.to_datetime(date_test_str, errors="raise", format="%Y-%m")
+    # Select data
+    df_train = df.loc[df[col_date] < date_valid]
+    df_valid = df.loc[(df[col_date] >= date_valid) & (df[col_date] < date_test)]
+    df_test = df.loc[df[col_date] >= date_test]
+    return df_train, df_valid, df_test
+
+
+def create_duration(df: pd.DataFrame, col_pickup_date: str="tpep_pickup_datetime", col_dropoff_date: str="tpep_dropoff_datetime") -> pd.DataFrame:
+    """Add duration time (in s) to the dataset using pickup and dropoff times.
+
+    Args:
+        df: DataFrame with pick-up and drop-off times
+        col_pickup_date: Name of the pickup datetime column
+        col_dropoff_date: Name of the dropoff datetime column
+    Returns:
+        df: DataFrame with the duration feature
+    """
+    # Convert datetime
+    df[col_pickup_date] = pd.to_datetime(df[col_pickup_date], errors='coerce')
+    df[col_dropoff_date] = pd.to_datetime(df[col_dropoff_date], errors='coerce')
+    # Compute duration
+    df['duration'] = (df[col_dropoff_date] - df[col_pickup_date]).dt.total_seconds()
+    return df
+
+
+def create_id(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    """
+    df = df.reset_index(drop=False).rename(columns={"index": "id"})
+    return df
