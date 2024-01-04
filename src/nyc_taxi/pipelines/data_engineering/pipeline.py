89d73e08aed8178c451742ca4861b2d98d0e10b4
BWallyn <ben96.wallyn@gmail.com>
2024-01-04 07:46:27 +0100

    delete dropoff date

--- src/nyc_taxi/pipelines/data_engineering/pipeline.py
+++ src/nyc_taxi/pipelines/data_engineering/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.19.1
 """
 
 from kedro.pipeline import node, Pipeline, pipeline
-from .nodes import download_between_dates, split_train_val_test, create_duration
+from .nodes import download_between_dates, split_train_val_test, create_duration, delete_dropoff_date
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -24,19 +24,37 @@ def create_pipeline(**kwargs) -> Pipeline:
         node(
             func=create_duration,
             inputs=["train_split"],
-            outputs="df_train",
+            outputs="df_train_target",
             name="node_target_train",
         ),
         node(
             func=create_duration,
             inputs=["valid_split"],
-            outputs="df_valid",
+            outputs="df_valid_target",
             name="node_target_valid",
         ),
         node(
             func=create_duration,
             inputs=["test_split"],
-            outputs="df_test",
+            outputs="df_test_target",
             name="node_target_test",
-        )
+        ),
+        node(
+            func=delete_dropoff_date,
+            inputs="df_train_target",
+            outputs="df_train",
+            name="node_remove_leak_train",
+        ),
+        node(
+            func=delete_dropoff_date,
+            inputs="df_valid_target",
+            outputs="df_valid",
+            name="node_remove_leak_valid",
+        ),
+        node(
+            func=delete_dropoff_date,
+            inputs="df_test_target",
+            outputs="df_test",
+            name="node_remove_leak_test",
+        ),
     ])
