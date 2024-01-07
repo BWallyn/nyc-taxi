f500073762f4300158e91a41deb9bf3645783107
BWallyn <ben96.wallyn@gmail.com>
2024-01-07 22:16:12 +0100

    create the pipeline estimator and add passthrough to imputer

--- src/nyc_taxi/pipelines/data_science/nodes.py
+++ src/nyc_taxi/pipelines/data_science/nodes.py
@@ -11,33 +11,37 @@ import numpy as np
 
 # Machine learning
 from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import HistGradientBoostingRegressor
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder
 
-from .feature_engineering import periodic_spline_transformer
+from feature_engineering import cos_transformer, sin_transformer, periodic_spline_transformer
 
 
 # ===================
 # ==== FUNCTIONS ====
 # ===================
 
-def sin_transformer(period):
-    return FunctionTransformer(lambda x: np.sin(x / period * 2 * np.pi))
-
-
-def cos_transformer(period):
-    return FunctionTransformer(lambda x: np.cos(x / period * 2 * np.pi))
-
 
 def column_transformer() -> ColumnTransformer:
-    """
+    """Create a column transformer
+
+    Returns:
+        col_transf: Column transformer element from sklearn
     """
     # Column transformer
     col_transf = ColumnTransformer(
         transformers=[
-            ("hour_sin", sin_transformer(24), ["tpep_pickup_datetime_hour"]),
-            ("hour_cos", sin_transformer(24), ["tpep_pickup_datetime_hour"]),
+            # ("hour_sin", sin_transformer(24), ["tpep_pickup_datetime_hour"]),
+            # ("hour_cos", cos_transformer(24), ["tpep_pickup_datetime_hour"]),
             ("hour_spline", periodic_spline_transformer(24, n_splines=12), ["tpep_pickup_datetime_hour"]),
+            ("ordinal_enc", OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=np.nan), ["store_and_fwd_flag"]),
+            ("ordinal_enc_max", OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=np.nan, max_categories=255), ["PULocationID", "DOLocationID"])
+            # ("passthrough_cols", "passthrough", [
+            #     "VendorID", "passenger_count", "trip_distance", "RatecodeID", "PULocationID", "DOLocationID", "payment_type", "fare_amount", "extra",
+            #     "mta_tax", "tip_amount", "tolls_amount", "improvement_surcharge", "total_amount", "congestion_surcharge", "airport_fee",
+            # ])
         ], remainder='passthrough', verbose_feature_names_out=False,
     )
     return col_transf
@@ -56,6 +60,31 @@ def feature_imputer():
             ("store_flag", SimpleImputer(strategy="most_frequent"), ["store_and_fwd_flag"]),
             ("cong_surch", SimpleImputer(strategy="median"), ["congestion_surcharge"]),
             ("air_fee", SimpleImputer(strategy="most_frequent"), ["airport_fee"]),
-        ], remainder='passthrough', verbose_feature_names_out=False
+            ("passthrough_cols", "passthrough", [
+                "VendorID", "trip_distance", "PULocationID", "DOLocationID",
+                "payment_type", "fare_amount", "extra",
+                "mta_tax", "tip_amount", "tolls_amount", "improvement_surcharge", "total_amount", "tpep_pickup_datetime_hour"
+            ])
+        ], remainder='drop', verbose_feature_names_out=False
     )
     return feat_imp
+
+
+def pipe_estimator(feat_imp: ColumnTransformer, col_transf: ColumnTransformer, **kwargs):
+    """Create a regressor estimator from sklearn using a pipeline
+
+    Args:
+        feat_imp: Feature imputer element
+        col_transf: Column transformer element
+        **kwargs: Hyperparameters for the HistGradientBoosting method
+    Returns:
+        estimator: Estimator element from sklearn
+    """
+    estimator = Pipeline(
+        steps=[
+            ('feature_imp', feat_imp),
+            ('column_transf', col_transf),
+            ('model', HistGradientBoostingRegressor(**kwargs)),
+        ]
+    ).set_output(transform="pandas")
+    return estimator
