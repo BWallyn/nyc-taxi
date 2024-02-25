ae6722d347011b9a1f14a92fc2a17cc5ce826f60
BWallyn <ben96.wallyn@gmail.com>
2024-02-25 22:39:06 +0100

    function to predict

--- src/nyc_taxi/pipelines/model_inference/nodes.py
+++ src/nyc_taxi/pipelines/model_inference/nodes.py
@@ -2,3 +2,23 @@
 This is a boilerplate pipeline 'model_inference'
 generated using Kedro 0.19.1
 """
+# =================
+# ==== IMPORTS ====
+# =================
+
+import pandas as pd
+
+from sklearn.pipeline import Pipeline
+
+
+# ===================
+# ==== FUNCTIONS ====
+# ===================
+
+def model_predict(df: pd.DataFrame, model: Pipeline) -> pd.Series:
+    """
+    """
+    pred = model.predict(df)
+    pred_df = pd.Series(pred, index=df.index)
+    return pred_df
+
