c292cc0289e1172475197a379f36b2b4bc0053a5
BWallyn <ben96.wallyn@gmail.com>
2024-02-25 22:40:15 +0100

    compute metrics and plot histograms

--- src/nyc_taxi/pipelines/model_evaluation/pipeline.py
+++ src/nyc_taxi/pipelines/model_evaluation/pipeline.py
@@ -3,8 +3,22 @@ This is a boilerplate pipeline 'model_evaluation'
 generated using Kedro 0.19.1
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import node, Pipeline, pipeline
+from .nodes import compute_metrics, plot_histograms
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=compute_metrics,
+            inputs=['y_training', 'pred_training', 'y_test', 'pred_test'],
+            outputs='dict_metrics',
+            name='node_metrics',
+        ),
+        node(
+            func=plot_histograms,
+            inputs=['y_test', 'pred_test'],
+            outputs=None,
+            name='node_plot_histograms_test',
+        )
+    ])
