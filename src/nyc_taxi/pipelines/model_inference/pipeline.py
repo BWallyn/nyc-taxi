bf5d583adcc7c5e2d3d211a5b9029f9926907e7c
BWallyn <ben96.wallyn@gmail.com>
2024-02-25 22:38:30 +0100

    add nodes to predict on training and test

--- src/nyc_taxi/pipelines/model_inference/pipeline.py
+++ src/nyc_taxi/pipelines/model_inference/pipeline.py
@@ -3,8 +3,22 @@ This is a boilerplate pipeline 'model_inference'
 generated using Kedro 0.19.1
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import node, Pipeline, pipeline
+from .nodes import model_predict
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=model_predict,
+            inputs=["df_training", "model_trained"],
+            outputs="pred_training",
+            name="node_model_predict_training",
+        ),
+        node(
+            func=model_predict,
+            inputs=["df_test_hour", "model_trained"],
+            outputs="pred_test",
+            name="node_model_predict_test",
+        ),
+    ])
