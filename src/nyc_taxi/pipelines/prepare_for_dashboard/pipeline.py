9944fcb3d835ead5af0cbb1e34558e6636f2565a
BWallyn <ben96.wallyn@gmail.com>
2024-04-27 19:10:57 +0200

    add preparation of geodataframe to pipeline

--- src/nyc_taxi/pipelines/prepare_for_dashboard/pipeline.py
+++ src/nyc_taxi/pipelines/prepare_for_dashboard/pipeline.py
@@ -7,6 +7,9 @@ from kedro.pipeline import node, Pipeline, pipeline
 from .nodes import (
     aggregate_by_day_hour,
     load_geographical_dataframe,
+    convert_crs,
+    get_representative_points,
+    get_lon_lat,
     add_geographical_info_pickup
 )
 
@@ -17,18 +20,36 @@ def create_pipeline(**kwargs) -> Pipeline:
             func=aggregate_by_day_hour,
             inputs=["df_training", "params:beg_specific_week"],
             outputs="df_training_group_dayofweek_hour",
-            name="node_aggregate_dayofweek_hour_training"
+            name="Aggregate_dayofweek_hour_training"
         ),
         node(
             func=load_geographical_dataframe,
             inputs="params:path_shape_locations",
             outputs="gdf_shape_locations",
-            name="node_load_geo_dataframe"
+            name="Load_geo_dataframe"
+        ),
+        node(
+            func=convert_crs,
+            inputs="gdf_shape_locations",
+            outputs="gdf_crs_changed",
+            name="Change_crs_to_4326",
+        ),
+        node(
+            func=get_representative_points,
+            inputs="gdf_crs_changed",
+            outputs="gdf_w_point",
+            name="Add_representative_points_polygon"
+        ),
+        node(
+            func=get_lon_lat,
+            inputs="gdf_w_point",
+            outputs="gdf_w_lon_lat",
+            name="Add_longitude_and_latitude"
         ),
         node(
             func=add_geographical_info_pickup,
-            inputs=["df_training_group_dayofweek_hour", "gdf_shape_locations"],
-            outputs="df_training_with_geo_info",
+            inputs=["df_training_group_dayofweek_hour", "gdf_w_lon_lat"],
+            outputs="gdf_training_with_geo_info",
             name="node_add_geographical_info"
         )
     ])
