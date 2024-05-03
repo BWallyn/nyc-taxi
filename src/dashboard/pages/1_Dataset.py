0b83771ce97098e8b7070c6487f2b93452556099
BWallyn <ben96.wallyn@gmail.com>
2024-05-03 12:20:48 +0200

    add docstrings

--- src/dashboard/pages/1_Dataset.py
+++ src/dashboard/pages/1_Dataset.py
@@ -8,6 +8,8 @@ import pandas as pd
 
 # Plots
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 import altair as alt
 
 # App
@@ -78,6 +80,57 @@ def plot_histogram(data_to_plot: np.array, name_group: list[str], title: str, x_
     st.pyplot(fig)
 
 
+def create_selectbox(tuple_locations: tuple[str]) -> str:
+    """Create a selectbox for the locations to display
+
+    Args:
+        tuple_locations (tuple[str]): List of the available locations
+    Returns:
+        (str): Selected location
+    """
+    return st.selectbox(
+        "Select location:",
+        tuple_locations
+    )
+
+
+@st.cache_data
+def select_location(df: pd.DataFrame, location_ex: str) -> pd.DataFrame:
+    """Filter the dataframe to select only the location selected by the selectbox
+
+    Args:
+        df (pd.DataFrame): Input dataframe
+        location_ex (str): Location selected
+    Returns:
+        (pd.DataFrame): Filtered dataframe
+    """
+    return df.loc[df["zone"] == location_ex]
+
+
+def plot_one_sample(
+    df: pd.DataFrame, location_ex: str,
+) -> go.Figure:
+    """Plot the number of trips in the time on a specific location
+
+    Args:
+        df (pd.DataFrame): Input dataframe
+        location_ex (str): Location displayed
+    Returns:
+        fig (go.Figure): Figure of the number of trips
+    """
+    # Define figure
+    fig = go.Figure()
+    title = f'Number of trip per hour for {location_ex}'
+    fig = px.line(
+        x=df['tpep_pickup_datetime'],
+        y=df['n_trips'],
+        template='plotly_dark',
+        markers=True,
+        title=title
+    )
+    return fig
+
+
 def plot_hist_data(df: pd.DataFrame,) -> None:
     """
     """
@@ -111,6 +164,13 @@ def create_body(path_data: str, path_target: str, path_data_by_day: str) -> None
     # Display the number of rides per hour
     st.write(f"**Number of rides per hour on the specific day {day_selected} chosen:**")
     plot_hist_data(df)
+    # Diplay the number of rides
+    df = load_data(path_file='data/08_reporting/df_training_group_location_datetime.pkl')
+    tuple_locations = tuple(set(df['zone'].values))
+    location_sel = create_selectbox(tuple_locations=tuple_locations)
+    df_sel = select_location(df, location_ex=location_sel)
+    fig = plot_one_sample(df_sel, location_ex=location_sel)
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True, width=1000)
 
 
 def main():
